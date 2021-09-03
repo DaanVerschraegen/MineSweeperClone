bb4dc7272f1c306ce39664a351b1f5ff508511dc
DaanVerschraegen <daan.verschraegen@outlook.com>
2021-09-03 17:27:13 +0200

    Initial commit

---
+++ Assets/MyAssets/Scripts/Grid/Gridmap.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gridmap
+{
+    private int width;
+    private int height;
+
+    public Gridmap(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
